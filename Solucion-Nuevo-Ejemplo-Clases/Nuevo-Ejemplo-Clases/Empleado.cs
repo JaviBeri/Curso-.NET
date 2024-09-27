2b29991845fbc5bf91e9cfaa52ef9aa55a14946b
JaviBeri <javiberi1981@gmail.com>
2024-09-27 12:25:25 +0200

    Hecho

--- Solucion-Nuevo-Ejemplo-Clases/Nuevo-Ejemplo-Clases/Empleado.cs
+++ Solucion-Nuevo-Ejemplo-Clases/Nuevo-Ejemplo-Clases/Empleado.cs
@@ -8,34 +8,23 @@ namespace Nuevo_Ejemplo_Clases
 {
     internal class Empleado
     {
-        public string Nombre
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        public string Departamento
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        public string Telefono
-        {
-            get => default;
-            set
-            {
-            }
-        }
+        public string Nombre { get; set; }
+       
+        public string Departamento { get; set; }
+        
+        public string Telefono { get; set; }
+        
         public int NumEmpleado { get; set; }
 
         public string NSS { get; set; }
 
         public string IBAN { get; set; }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"Nombre empleado: {Nombre}, Departamento: {Departamento}, Teléfono: {Telefono}, " +
+                $"Número de empleado: {NumEmpleado}, IBAN: {IBAN}, NSS: {NSS}");
+        }
 
     }
 }
--- Solucion-Nuevo-Ejemplo-Clases/Nuevo-Ejemplo-Clases/Program.cs
+++ Solucion-Nuevo-Ejemplo-Clases/Nuevo-Ejemplo-Clases/Program.cs
@@ -13,24 +13,44 @@
             Pedro.Nombre = "Pedro";
             Pedro.Departamento = "IT";
             Pedro.Telefono = "666666666";
+            Pedro.NumEmpleado = 1234;
+            Pedro.IBAN = "63754237289";
+            Pedro.NSS = "9873438635";
+
+            Pedro.MostrarDatos();
                 
             Empleado Maria = new Empleado();
 
             Maria.Nombre = "Maria";
             Maria.Departamento = "Desarrollo Apps";
             Maria.Telefono = "222222222";
+            Maria.NumEmpleado = 6789;
+            Maria.IBAN = "12987863532";
+            Maria.NSS = "5463882526";
+
+            Maria.MostrarDatos();
 
             Empleado Juan = new Empleado();
 
             Juan.Nombre = "Juan";
             Juan.Departamento = "Recursos Humanos";
             Juan.Telefono = "444444444";
+            Juan.NumEmpleado = 4321;
+            Juan.IBAN = "98353747698";
+            Juan.NSS = "3984534732";
+
+            Juan.MostrarDatos();
 
             Empleado Javier = new Empleado();
 
             Javier.Nombre = "Javier";
             Javier.Departamento = "Soporte Herramientas";
             Javier.Telefono = "888888888";
+            Javier.NumEmpleado = 4567;
+            Javier.IBAN = "926364378465";
+            Javier.NSS = "256145241";
+
+            Javier.MostrarDatos();
 
         }
     }
