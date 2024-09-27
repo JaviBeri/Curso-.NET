2a26d8dcad4c11bc56646c31b7d23843c6f3408f
JaviBeri <javiberi1981@gmail.com>
2024-09-27 10:36:01 +0200

    Cambios

---
+++ Solucion-Nuevo-Ejemplo-Clases/Nuevo-Ejemplo-Clases/Empleado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuevo_Ejemplo_Clases
+{
+    internal class Empleado
+    {
+        public string Nombre
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Departamento
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Telefono
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        public int NumEmpleado { get; set; }
+
+        public string NSS { get; set; }
+
+        public string IBAN { get; set; }
+
+    }
+}
---
+++ Solucion-Nuevo-Ejemplo-Clases/Nuevo-Ejemplo-Clases/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Nuevo_Ejemplo_Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Crear las instancias de los empleados
+
+            Empleado Pedro = new Empleado();
+
+            Pedro.Nombre = "Pedro";
+            Pedro.Departamento = "IT";
+            Pedro.Telefono = "666666666";
+                
+            Empleado Maria = new Empleado();
+
+            Maria.Nombre = "Maria";
+            Maria.Departamento = "Desarrollo Apps";
+            Maria.Telefono = "222222222";
+
+            Empleado Juan = new Empleado();
+
+            Juan.Nombre = "Juan";
+            Juan.Departamento = "Recursos Humanos";
+            Juan.Telefono = "444444444";
+
+            Empleado Javier = new Empleado();
+
+            Javier.Nombre = "Javier";
+            Javier.Departamento = "Soporte Herramientas";
+            Javier.Telefono = "888888888";
+
+        }
+    }
+}
