82d2db450cba61b287a13f3dfc04a42594db7984
Usuario - <Usuario@DESKTOP-H7VOC98>
2024-10-12 19:49:30 +0200

    Seguimos

--- Solucion-Calculadora/Calculadora-Ejemplo-Clase/Calculadora.cs
+++ Solucion-Calculadora/Calculadora-Ejemplo-Clase/Calculadora.cs
@@ -15,9 +15,32 @@ namespace Calculadora_Ejemplo_Clase
         }
 
         // Método que resta dos números
-        public double Restar(double num1, double num2)
+        public double Restar(double rest1, double rest2)
         {
-            return num1 - num2;
+            return rest1 - rest2;
+        }
+
+        // Método que divide dos números
+        public double Dividir(double div1, double div2)
+        {
+            if (div2 == 0)
+            {
+                MostrarMensajes("No se puede dividir por cero");
+                return 0;
+            }
+            return div1 / div2;
+        }
+
+        // Método que multiplica dos números
+        public double Multi(double mul1, double mul2)
+        {
+            return mul1 * mul2;
+        }
+        
+        // Método que muestra un mensaje por pantalla
+        public void MostrarMensajes(string mensaje)
+        {
+            Console.WriteLine(mensaje);
         }
     }
 }
--- Solucion-Calculadora/Calculadora-Ejemplo-Clase/Program.cs
+++ Solucion-Calculadora/Calculadora-Ejemplo-Clase/Program.cs
@@ -4,7 +4,74 @@
 Calculadora calculadora = new Calculadora();
 
 // Llamar al método Sumar de la calculadora
-double resultado = calculadora.Sumar(5, 10);
+// double resultado = calculadora.Sumar(5, 10);
 
 // Mostrar el resultado
-Console.WriteLine(resultado);
+// Console.WriteLine(resultado);
+
+// Menú para la calculadora
+bool salir = false;
+while (!salir)
+{
+    
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("Selecciona una opción: ");
+    string? opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        // Suma de dos números
+        case "1":
+            Console.WriteLine("Introduce el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double suma = calculadora.Sumar(num1, num2);
+            calculadora.MostrarMensajes($"El resultado de la suma es: {suma}");
+            break;
+
+        // Resta de dos números
+        case "2":
+            Console.WriteLine("Introduce el primer número: ");
+            double rest1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número: ");
+            double rest2 = Convert.ToDouble(Console.ReadLine());
+            double resta = calculadora.Restar(rest1, rest2);
+            calculadora.MostrarMensajes($"El resultado de la resta es: {resta}");
+            break;
+
+        // Multiplicación de dos números
+        case "3":
+            Console.WriteLine("Introduce el primer número: ");
+            double mul1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número: ");
+            double mul2 = Convert.ToDouble(Console.ReadLine());
+            double multi = calculadora.Multi(mul1, mul2);
+            calculadora.MostrarMensajes($"El resultado de la resta es: {multi}");
+            break;
+
+        // División de dos números
+        case "4":
+            Console.WriteLine("Introduce el primer número: ");
+            double div1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número: ");
+            double div2 = Convert.ToDouble(Console.ReadLine());
+            double divid = calculadora.Dividir(div1, div2);
+            calculadora.MostrarMensajes($"El resultado de la resta es: {divid}");
+            break;
+            
+        case "5":
+            salir = true;
+            calculadora.MostrarMensajes("Hasta pronto!!");
+            break;
+
+        default:
+            calculadora.MostrarMensajes("Opción no válida, selecciona de nuevo: ");
+            break;
+
+    }
+}
