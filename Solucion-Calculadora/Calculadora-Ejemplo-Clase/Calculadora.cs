bdb218186386c2c9647599d6712d71a449d187c8
Usuario - <Usuario@DESKTOP-H7VOC98>
2024-10-13 12:15:57 +0200

    Modificado

--- Solucion-Calculadora/Calculadora-Ejemplo-Clase/Calculadora.cs
+++ Solucion-Calculadora/Calculadora-Ejemplo-Clase/Calculadora.cs
@@ -8,16 +8,29 @@ namespace Calculadora_Ejemplo_Clase
 {
     public class Calculadora
     {
+        // Definir campo para almacenar resultado
+        private double memoria = 0;
+
+        // Definir propiedad para acceder y guardar en la memoria
+        public double MemoriaCalculadora
+        {
+            get; set;
+        }
+
         // Método que suma dos números
-        public double   Sumar(double num1, double num2)
+        public double Sumar(double num1, double num2)
         { 
-            return num1 + num2; 
+            double resultado = num1 + num2;
+            memoria = resultado;
+            return resultado; 
         }
 
         // Método que resta dos números
         public double Restar(double rest1, double rest2)
         {
-            return rest1 - rest2;
+            double resultado = rest1 - rest2;
+            memoria = resultado;
+            return resultado;
         }
 
         // Método que divide dos números
@@ -28,19 +41,48 @@ namespace Calculadora_Ejemplo_Clase
                 MostrarMensajes("No se puede dividir por cero");
                 return 0;
             }
+            double resultado = div1 / div2;
+            memoria = resultado;
             return div1 / div2;
         }
 
         // Método que multiplica dos números
         public double Multi(double mul1, double mul2)
         {
-            return mul1 * mul2;
+            double resultado = mul1 * mul2;
+            memoria = resultado;
+            return resultado;
         }
         
         // Método que muestra un mensaje por pantalla
         public void MostrarMensajes(string mensaje)
         {
             Console.WriteLine(mensaje);
+        }
+
+        // Método que guarda en memoria o bien un valor pasado como parametro
+        public void GuardarEnMemoria(double valor = 0)
+        {
+            memoria = valor;
+        }
+
+        // Método para obtener un valor numérico de la consola
+        // y devolverlo como doble para evitar que el programa 
+        // se bloquee si no es numérico
+        public double ObtenerNumeroConsola()
+        {
+            double numero = 0;
+            bool esNumero = false;
+            while (!esNumero)
+            {
+                string? valor = Console.ReadLine();
+                esNumero = double.TryParse(valor, out numero);
+                if (!esNumero)
+                {
+                    MostrarMensajes("El valor introducido no es un número. Inténtalo de nuevo");
+                }
+            }
+            return numero;
         }
     }
 }
--- Solucion-Calculadora/Calculadora-Ejemplo-Clase/Program.cs
+++ Solucion-Calculadora/Calculadora-Ejemplo-Clase/Program.cs
@@ -18,7 +18,8 @@ while (!salir)
     Console.WriteLine("2. Restar");
     Console.WriteLine("3. Multiplicar");
     Console.WriteLine("4. Dividir");
-    Console.WriteLine("5. Salir");
+    Console.WriteLine("5. Guardar en memoria");
+    Console.WriteLine("9. Salir");
     Console.WriteLine("Selecciona una opción: ");
     string? opcion = Console.ReadLine();
 
@@ -27,9 +28,9 @@ while (!salir)
         // Suma de dos números
         case "1":
             Console.WriteLine("Introduce el primer número: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num1 = calculadora.ObtenerNumeroConsola();
             Console.WriteLine("Introduce el segundo número: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            double num2 = calculadora.ObtenerNumeroConsola(); 
             double suma = calculadora.Sumar(num1, num2);
             calculadora.MostrarMensajes($"El resultado de la suma es: {suma}");
             break;
@@ -37,9 +38,9 @@ while (!salir)
         // Resta de dos números
         case "2":
             Console.WriteLine("Introduce el primer número: ");
-            double rest1 = Convert.ToDouble(Console.ReadLine());
+            double rest1 = calculadora.ObtenerNumeroConsola();
             Console.WriteLine("Introduce el segundo número: ");
-            double rest2 = Convert.ToDouble(Console.ReadLine());
+            double rest2 = calculadora.ObtenerNumeroConsola();
             double resta = calculadora.Restar(rest1, rest2);
             calculadora.MostrarMensajes($"El resultado de la resta es: {resta}");
             break;
@@ -47,9 +48,9 @@ while (!salir)
         // Multiplicación de dos números
         case "3":
             Console.WriteLine("Introduce el primer número: ");
-            double mul1 = Convert.ToDouble(Console.ReadLine());
+            double mul1 = calculadora.ObtenerNumeroConsola();
             Console.WriteLine("Introduce el segundo número: ");
-            double mul2 = Convert.ToDouble(Console.ReadLine());
+            double mul2 = calculadora.ObtenerNumeroConsola();
             double multi = calculadora.Multi(mul1, mul2);
             calculadora.MostrarMensajes($"El resultado de la resta es: {multi}");
             break;
@@ -57,14 +58,19 @@ while (!salir)
         // División de dos números
         case "4":
             Console.WriteLine("Introduce el primer número: ");
-            double div1 = Convert.ToDouble(Console.ReadLine());
+            double div1 = calculadora.ObtenerNumeroConsola();
             Console.WriteLine("Introduce el segundo número: ");
-            double div2 = Convert.ToDouble(Console.ReadLine());
+            double div2 = calculadora.ObtenerNumeroConsola();
             double divid = calculadora.Dividir(div1, div2);
             calculadora.MostrarMensajes($"El resultado de la resta es: {divid}");
             break;
-            
+
         case "5":
+            calculadora.GuardarEnMemoria();
+            calculadora.MostrarMensajes($"El valor {calculadora.MemoriaCalculadora} se ha guardado");
+            break;
+            
+        case "9":
             salir = true;
             calculadora.MostrarMensajes("Hasta pronto!!");
             break;
