904294a048ed343dfa2e04cc9536faa92a6f11d0
Usuario - <Usuario@DESKTOP-H7VOC98>
2024-10-13 20:00:26 +0200

    Clases

---
+++ Solucion-Ejemplo-Herencia/Clases-Animales/GatoDoméstico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases_Animales
+{
+    public class GatoDoméstico : Gato
+    {
+    }
+}
---
+++ Solucion-Ejemplo-Herencia/ClasesLibros/Libros.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesLibros
+{
+    public abstract class Libros
+    { 
+    
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+
+        public int Paginas { get; set; }
+
+        public Libros(string titulo, string autor, int paginas)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Paginas = paginas;
+        }
+
+        public abstract void MostrarInfo();
+    }
+}
